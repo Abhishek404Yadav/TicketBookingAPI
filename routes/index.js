9add39e8bb93f5406951e9de7b36e1ee103caa15
Abhishek Yadav <abhiyadav.ce@gmail.com>
2023-08-18 16:40:08 +0530

    completed the project

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,6 @@ const screenRoute = require("./screen.route");
 router.use("/screen",screenRoute);
 
 const seatRoute = require("./seat.route");
-router.use("./:screenId/:timingId/seat",seatRoute);
+router.use("/seat/:screenId/:timingId",seatRoute);
 
 module.exports = router;
--- routes/seat.route.js
+++ routes/seat.route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const seatController = require('../controller/seat.controller');
 
+router.get('/',seatController.getAvailableSeat);
 router.post('/',seatController.bookSeat);
-router.get('/',seatController.getAvailabelSeat);
+router.post('/add',seatController.addSeat);
 
 module.exports = router;
